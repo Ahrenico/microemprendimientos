1986c35fbb004b151451b0fbb3a2f863d2fa0428
Matias Leandro <matiasleandroheredia@gmail.com>
2024-06-01 00:05:15 -0300

    arreglos en la busqueda y ordenamientos de emprendimientos

--- busqueda.js
+++ busqueda.js
@@ -1,23 +1,306 @@
+var datos = {
+    "colaboradores": [
+      {
+        "id": 0,
+        "nombre": "Marcos",
+        "apellido": "Sosa",
+        "telefono": 1155555555,
+        "contraseña": "123456789",
+        "email": "mSosa@gmail.com",
+        "emprendimiento": [{
+          "nombre": "Veterinaria Invet 24 hs",
+          "descripcion": "Veterinaria",
+          "rubro": "Mascotas",
+          "direccion": "Av Dr. Ricardo Balbin 660, San Miguel",
+          "coordenadas": [-34.53791, -58.70785],
+          "contacto": 1155555555,
+          "redes_sociales": "instagram.com/Veterinaria",
+          "formas_de_pago": "efectivo o tarjeta",
+          "zona_de_influencia": "",
+          "restricciones": "",
+          "foto": "images/emprendimientos/Veterinaria Invet.jpg",
+          "horarios": "24Hs",
+          "visibilidad": true,
+          "destacado": true
+        }]
+      },
+      {
+        "id": 1,
+        "nombre": "Bruno",
+        "apellido": "Ambrosoni",
+        "telefono": 1162739935,
+        "contraseña": "sd",
+        "email": "bambrosoni@gmail.com",
+        "emprendimiento": [{
+          "nombre": "Alma de Barrio",
+          "descripcion": "Restaurante",
+          "rubro": "Comida",
+          "direccion": "Italia 894 Esquina, San Miguel",
+          "coordenadas": [-34.541578991192374, -58.70603526034107],
+          "contacto": "1162739935",
+          "redes_sociales": "redes",
+          "formas_de_pago": "efectivo o tarjeta",
+          "zona_de_influencia": "",
+          "restricciones": "",
+          "foto": "images/emprendimientos/Alma de Barrio.JPG",
+          "horarios": "8:30hs a 21hs",
+          "visibilidad": true,
+          "destacado": true
+        }]
+      },
+      {
+        "id": 2,
+        "nombre": "Daenerys",
+        "apellido": "Targaryen",
+        "telefono": 1136375151,
+        "contraseña": "",
+        "email": "dtargaryen@cemic.edu.ar",
+        "emprendimiento": [{
+          "nombre": "LA FACU",
+          "descripcion": "Complejo deportivo",
+          "rubro": "Deportes y recreacion",
+          "direccion": "Juan Maria Gutierrez 960, Los Polvorines",
+          "coordenadas": [-34.52273978533054, -58.70224413377293],
+          "contacto": 1136375151,
+          "redes_sociales": "instagram.com/LAFACU",
+          "formas_de_pago": "Solo efectivo",
+          "zona_de_influencia": "",
+          "restricciones": "",
+          "foto": "images/emprendimientos/La Facu.jpg",
+          "horarios": "10am a 9pm",
+          "visibilidad": true,
+          "destacado": true
+        }]
+      }
+      ,
+      {
+        "id": 3,
+        "nombre": "Carlos",
+        "apellido": "Aquino",
+        "telefono": 1555556666,
+        "contraseña": "sdf",
+        "email": "caquino@gmail.com",
+        "emprendimiento": [{
+          "nombre": "Macetas La Alemana",
+          "descripcion": "Floreria",
+          "rubro": "Floreria",
+          "direccion": "Juan María Gutiérrez 533,  Los Polvorines",
+          "coordenadas": [-34.5251364865928, -58.70545935343455],
+          "contacto": 1555556666,
+          "redes_sociales": "@MacetasLaAlemana",
+          "formas_de_pago": "Efectivo y tarjeta",
+          "zona_de_influencia": "",
+          "restricciones": "No se hacen envios",
+          "foto": "images/emprendimientos/Macetas La Alemana.jpg",
+          "horarios": "10am a 21pm",
+          "visibilidad": true,
+          "destacado": true
+        }]
+      }
+      ,
+      {
+        "id": 4,
+        "nombre": "Juan",
+        "apellido": "Perez",
+        "telefono": 1161753441,
+        "contraseña": "asdf",
+        "email": "jperez@gmail.com",
+        "emprendimiento": [{
+          "nombre": "cerrajero Malvinas Argentinas",
+          "descripcion": "CERRAJERO. Soluciones de cerrajería - Blindajes - Seguridad Electrónica",
+          "rubro": "Cerrajeria",
+          "direccion": "Juan Maria Gutierrez 621, Los Polvorines",
+          "coordenadas": [-34.52469872723147, -58.704707710075546],
+          "contacto": 1161753441,
+          "redes_sociales": "cerrajeromalvinasargentinas.com.ar",
+          "formas_de_pago": "Solo efectivo",
+          "zona_de_influencia": "",
+          "restricciones": "",
+          "foto": "images/emprendimientos/Cerrajero Malvinas Argentinas.jpg",
+          "horarios": "abierto las 24hs",
+          "visibilidad": true,
+          "destacado": true
+        }]
+      }
+      ,
+      {
+        "id": 5,
+        "nombre": "Paulina",
+        "apellido": "Rubio",
+        "telefono": 1146641720,
+        "contraseña": "",
+        "email": "prubio@gmail.com",
+        "emprendimiento": [{
+          "nombre": "Don Valentín Parrilla",
+          "descripcion": "Parrilla",
+          "rubro": "Comida",
+          "direccion": "Av. Pres. Arturo Umberto Illia 3873, San Miguel",
+          "coordenadas": [-34.5318347114765, -58.703024319464795],
+          "contacto": 1146641720,
+          "redes_sociales": "donvalentinparrilla.com.ar/",
+          "formas_de_pago": "Efectivo y tarjeta",
+          "zona_de_influencia": "Se hacen envios a todo San Miguel",
+          "restricciones": "",
+          "foto": "images/emprendimientos/Don Valentin Parrilla.jpg",
+          "horarios": "11:30am a 1am",
+          "visibilidad": true,
+          "destacado": true
+        }]
+      }
+      ,
+      {
+        "id": 6,
+        "nombre": "Dylan",
+        "apellido": "Massa",
+        "telefono": 1155556655,
+        "contraseña": "sdf",
+        "email": "dmassa@gmail.com",
+        "emprendimiento": [{
+          "nombre": "Kangaroo Gym",
+          "descripcion": "gimnasio",
+          "rubro": "Deportes y recreacion",
+          "direccion": "Rosetti 1690, San Miguel",
+          "coordenadas": [-34.53363978127867, -58.70531659577647],
+          "contacto": 1155556655,
+          "redes_sociales": "https://instagram.com/kangaroogym",
+          "formas_de_pago": "Solo efectivo",
+          "zona_de_influencia": "",
+          "restricciones": "",
+          "foto": "images/emprendimientos/Kangaroo Gym.jpg",
+          "horarios": "8am a 10pm",
+          "visibilidad": true,
+          "destacado": false
+        }]
+      },
+      {
+        "id": 7,
+        "nombre": "Marco Antonio",
+        "apellido": "Solis",
+        "telefono": 1144510894,
+        "contraseña": "sad",
+        "email": "masolis@gmail.com",
+        "emprendimiento": [{
+          "nombre": "Futbol La Esquina",
+          "descripcion": "Canchas para alquilar",
+          "rubro": "Deportes y recreacion",
+          "direccion": "Italia 1813, San Miguel",
+          "coordenadas": [-34.54827734100114, -58.71324953027017],
+          "contacto": 1144510894,
+          "redes_sociales": "instagram.com/laEsquina",
+          "formas_de_pago": "Solo efectivo",
+          "zona_de_influencia": "",
+          "restricciones": "",
+          "foto": "images/emprendimientos/Futbol La esquina.jpg",
+          "horarios": "12pm a 12am",
+          "visibilidad": true,
+          "destacado": false
+        }]
+      },
+      {
+        "id": 8,
+        "nombre": "Carlos",
+        "apellido": "Garcia",
+        "telefono": 1146668609,
+        "contraseña": "sadf",
+        "email": "cgarcia@gmail.com",
+        "emprendimiento": [{
+          "nombre": "Peluquerias Amadeus",
+          "descripcion": "Peluqueria",
+          "rubro": "Belleza",
+          "direccion": "Dr. Pedro A. Pardo 1579, Muñiz",
+          "coordenadas": [-34.556020441691146, -58.70004870509074],
+          "contacto": 1146668609,
+          "redes_sociales": "instagram.com/amadeus",
+          "formas_de_pago": "Efectivo y Tarjeta",
+          "zona_de_influencia": "",
+          "restricciones": "Solo con turnos",
+          "foto": "images/emprendimientos/Peluqueria Amadeus.jpeg",
+          "horarios": "10am a 20pm",
+          "visibilidad": false,
+          "destacado": false
+        }]
+      },
+      {
+        "id": 9,
+        "nombre": "Maria",
+        "apellido": "Becerra",
+        "telefono": 1155555555,
+        "contraseña": "",
+        "email": "mBecerra@gmail.com",
+        "emprendimiento": [{
+          "nombre": "Almacen Martin",
+          "descripcion": "Almacen",
+          "rubro": "Supermercados",
+          "direccion": "Eustoquio Frias 3783, Los Polvorines",
+          "coordenadas": [-34.50236146656121, -58.70211245374574],
+          "contacto": 1155555555,
+          "redes_sociales": "-",
+          "formas_de_pago": "Efectivo y tarjeta",
+          "zona_de_influencia": "",
+          "restricciones": "",
+          "foto": "images/emprendimientos/Almacen Martin.jpg",
+          "horarios": "9hs a 14hs y 17hs a 22hs",
+          "visibilidad": true,
+          "destacado": false
+        }]
+      },
+      {
+        "id": 10,
+        "nombre": "Valentin",
+        "apellido": "Perez",
+        "telefono": 1140573494,
+        "contraseña": "sdfa",
+        "email": "vperez@gmail.com",
+        "emprendimiento": [{
+          "nombre": "Canchas Lemos FC",
+          "descripcion": "Canchas para alquilar",
+          "rubro": "Deportes y recreacion",
+          "direccion": "Au Pres. Arturo Umberto Illia 3530, San Miguel",
+          "coordenadas": [-34.534124009428204, -58.699556011276464],
+          "contacto": 1140573494,
+          "redes_sociales": "instagram.com/lemosFC",
+          "formas_de_pago": "Solo efectivo",
+          "zona_de_influencia": "",
+          "restricciones": "",
+          "foto": "images/emprendimientos/Canchas Lemos FC.jpg",
+          "horarios": "12am a 11:30pm",
+          "visibilidad": true,
+          "destacado": false
+        }]
+      }
+  
+  
+  
+    ]
+  }
+
 document.addEventListener("DOMContentLoaded", function() {
-    let inputBusqueda = document.getElementById("search")
+    let inputBusqueda = document.getElementById("search");
     let lista = document.getElementById("sugerencias");
 
-
     inputBusqueda.addEventListener("input", function() {
-        if(inputBusqueda.value == ""){
+        if (inputBusqueda.value == "") {
             lista.innerHTML = "";
-        }
-        else {
+        } else {
             let busqueda = inputBusqueda.value.toLowerCase();
-            fetch('emprendimientos.json')
-            .then(response => response.json())
-            .then(data => {
-                let resultados = data.filter(item => item.colaborador.emprendimiento.nombre.toLowerCase().includes(busqueda));
-                let resultadosOrdenados = resultados.sort((a, b) => b.colaborador.emprendimiento.destacado - a.colaborador.emprendimiento.destacado);
-                insertarListaSugerencia(resultadosOrdenados);
-            }) 
+            
+            let resultados = datos.colaboradores.filter(colaborador => 
+                colaborador.emprendimiento.some(emprendimiento => 
+                    emprendimiento.nombre.toLowerCase().includes(busqueda)
+                )
+            );
+
+            let resultadosOrdenados = resultados.sort((a, b) => {
+                let aDestacado = a.emprendimiento.some(e => e.destacado);
+                let bDestacado = b.emprendimiento.some(e => e.destacado);
+                if (aDestacado && !bDestacado) return -1;
+                if (!aDestacado && bDestacado) return 1;
+                return 0;
+            });
+
+            insertarListaSugerencia(resultadosOrdenados);
         }
-    })
+    });
 });
 
 function insertarListaSugerencia(resultados) {
@@ -25,21 +308,20 @@ function insertarListaSugerencia(resultados) {
     lista.innerHTML = '';
 
     if (resultados.length > 0) {
-        resultados.forEach(item => {
-            let listItem = document.createElement("li");
-            listItem.append(item.colaborador.emprendimiento.nombre
-                + ', ' +
-                item.colaborador.emprendimiento.direccion);
-            if(item.colaborador.emprendimiento.destacado){
-                let itemDestacado = document.createElement("span");
-                let itemDestacadoIcon = document.createElement("i");
-                itemDestacadoIcon.classList.add("fa-regular");
-                itemDestacadoIcon.classList.add("fa-star");
-                itemDestacado.append(itemDestacadoIcon);
-                listItem.append(itemDestacado);
-                listItem.classList.add("item-destacado");
-            }
-            lista.append(listItem);
+        resultados.forEach(colaborador => {
+            colaborador.emprendimiento.forEach(emprendimiento => {
+                let listItem = document.createElement("li");
+                listItem.append(emprendimiento.nombre + ', ' + emprendimiento.direccion);
+                if (emprendimiento.destacado) {
+                    let itemDestacado = document.createElement("span");
+                    let itemDestacadoIcon = document.createElement("i");
+                    itemDestacadoIcon.classList.add("fa-regular", "fa-star");
+                    itemDestacado.append(itemDestacadoIcon);
+                    listItem.append(itemDestacado);
+                    listItem.classList.add("item-destacado");
+                }
+                lista.append(listItem);
+            });
         });
     } else {
         let listItem = document.createElement("li");
